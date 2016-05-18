c5f9a1efbddac1f1dd207faf5ca9da5170b3548b
hoyassassin <csalg808@gmail.com>
2016-05-18 14:53:44 -0400

    first commit

---
+++ helloworld/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("where in the world do i go next")
+	fmt.Println("hellow world!" +
+		"you suck" +
+		"yes")
+
+	fmt.Print("what up yall	")
+}
